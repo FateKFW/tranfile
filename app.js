a971d37bf423aaff7d95fe16bb0700ddae6b4ede
rdz <942311090@qq.com>
2020-03-24 22:17:14 +0800

    解决跨机器不能访问的BUG

--- app.js
+++ app.js
@@ -7,14 +7,24 @@ const conf = require('./conf/config.js')
 const fu = require('./util/fileUtil.js')
 const ip = require('./util/ipUtil.js')
 
-
 var app = express()
 .use(body.json())
 .use(body.urlencoded({ extended: true }))
 
+//设置跨域访问  
+app.all('*', function(req, res, next) {  
+    res.header("Access-Control-Allow-Origin", "*");  
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");  
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
+    res.header("X-Powered-By",' 3.2.1')  
+    res.header("Content-Type", "application/json;charset=utf-8");  
+    next();  
+}); 
+
+//访问首页
 app.get('/', (req, res) => {
     //console.info(req.query)
-    //console.info(req.host+"请求URL:"+req.url)
+    console.info(req.host+"请求URL:"+req.url)
     res.writeHeader(200, {"Content-Type": "text/html"});
     fs.readFile("./front/page/index.html",function (err,data){
         if(err){
@@ -22,13 +32,15 @@ app.get('/', (req, res) => {
             res.write('<h1>404</h1>');
         } else{
             res.write(data);
-            res.write(`<script>[appPort,path] = ["${conf.port}","${conf.address}"]</script>`)
+            res.write(`<script>path = "${conf.address}"</script>`)
         }
         res.end();
     });
 });
 
+//前台资源访问
 app.get('/front/*', (req, res) => {
+    console.info(req.host+"请求URL:"+req.url)
     res.writeHeader(200, {"Content-Type": "application/octet-stream"});
     fs.readFile("."+req.url,function (err,data){
         if(err){
@@ -41,13 +53,17 @@ app.get('/front/*', (req, res) => {
     });
 });
 
+//文件列表
 app.post("/fslist",(req,res)=>{
+    console.info(req.host+"请求URL:"+req.url)
     let params = req.body
     res.write(JSON.stringify(fu(params.path)))
     res.end()
 })
 
+//下载
 app.get("/download",(req,res)=>{
+    console.info(req.host+"请求URL:"+req.url)
     let params = req.query
     let f = fs.createReadStream(params.path)
     res.writeHead(200, {
